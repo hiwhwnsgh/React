c54da31b25454fe966b1d39d3831bdbb39c289b4
JunHo <hiwhwnsgh@gmail.com>
2023-06-21 01:03:32 +0900

    17) 리덕스를 사용하여 리액트 애플리케이션 상태 관리하기 - 1

---
+++ react-redux-tutorial/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { createStore} from 'redux';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from '@redux-devtools/extension';
+
+// 스토어 만들기
+const store = createStore(rootReducer, devToolsEnhancer);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+      <App/>
+  </Provider>      
+);
